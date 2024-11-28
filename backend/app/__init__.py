d8d7721b88c3700feced7facf8e2d806fffcc55a
savvaniss <savvaniss@yahoo.gr>
2024-11-28 15:41:13 +0200

    allow cors on backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,10 @@
 # app/__init__.py
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Import routes to register them with the app
 from app import routes
